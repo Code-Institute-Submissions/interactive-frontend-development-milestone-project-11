c7fbcf1d9e4c1cdbf17636032b6c75ef2ff3fb71
ErikaN-ir <erika.nethaway@gmail.com>
2020-04-05 20:07:46 +0000

    HTML wordspace divs set

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,47 +48,13 @@ function loadFlashcard(displayWords) {
     // document.getElementById("start").addEventListener("click", function(e) {
     //    showGameboard(displayWords);
     //});
-    startGameboard(displayWords);
+   // startGameboard(displayWords);
 }
 
 // Show Gameboard function
 function showGameboard() {
     document.getElementById("flashcard").style.display = "none";
     document.getElementById("gameboard").style.display = "block";
-}
-
-// Populating Gameboard with Word numbers
-function startGameboard(displayWords) {
-    console.log(displayWords);
-    var e = "";
-    for (var i=0; i<displayWords.length; i++) {
-     e += "<div id='wordSpace" + i + "'>" + " " + (i+1) + ". " + "</div>";
-     document.getElementById("gameboard").innerHTML = e;          
-    }
-    startForm(displayWords);
-}
-
-// Start first form
-function startForm(displayWords) {
-    var wordNum = 0;
-
-    function loadForm() {
-        var form = "";
-        var btn = "";
-        //if less than array length, generate the form and button
-        if (wordNum < displayWords.length) {
-             form += "<input type='text'" + "Text" + "</input>";
-             btn += "<input type='button' id='check-answer' value='Check'" + "Text" + "</input>";
-             ++wordNum;
-        } else {
-            alert("Something went wrong");
-        }
-    }
-    loadForm();
-
-    //document.getElementById("check-answer").addEventListener("click", function(e) {
-     //   loadForm();
-    //});
 }
 
 
