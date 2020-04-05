3c785ecee4750d39a2e3157100d2a89e6407363e
ErikaN-ir <erika.nethaway@gmail.com>
2020-04-05 20:44:03 +0000

    Displaying wordspaces

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,7 +48,7 @@ function loadFlashcard(displayWords) {
     // document.getElementById("start").addEventListener("click", function(e) {
     //    showGameboard(displayWords);
     //});
-   // startGameboard(displayWords);
+   startGameboard(displayWords);
 }
 
 // Show Gameboard function
@@ -57,9 +57,59 @@ function showGameboard() {
     document.getElementById("gameboard").style.display = "block";
 }
 
+//Displaying Words on Gameboard
+// Only show the number of words in the array
+// Use index number to get Id of element
+
+function startGameboard (displayWords) {
+    for (var i=0; i<displayWords.length; i++) {
+        var e = "";
+        e += "word" + (i+1);
+        console.log(e);
+        document.getElementById(e).style.display = "block";
+    }
+}
+
+
+
+/*
+// Populating Gameboard with Word numbers
+function startGameboard(displayWords) {
+    console.log(displayWords);
+    var e = "";
+    for (var i=0; i<displayWords.length; i++) {
+     e += "<div id='wordSpace" + i + "'>" + " " + (i+1) + ". " + "</div>";
+     document.getElementById("gameboard").innerHTML = e;          
+    }
+    startForm(displayWords);
+}
+
+// Start first form
+function startForm(displayWords) {
+    var wordNum = 0;
+
+    function loadForm() {
+        var form = "";
+        var btn = "";
+        //if less than array length, generate the form and button
+        if (wordNum < displayWords.length) {
+             form += "<input type='text'>" + "Text" + "</input>";
+             btn += "<input type='button' id='check-answer' value='Check'>" + "Text" + "</input>";
+             ++wordNum;
+        } else {
+            alert("Something went wrong");
+        }
+    }
+    loadForm();
+
+    //document.getElementById("check-answer").addEventListener("click", function(e) {
+     //   loadForm();
+    //});
+}
+*/
 
 // 
 
 // Level function
 
-// Round function
+// Round
